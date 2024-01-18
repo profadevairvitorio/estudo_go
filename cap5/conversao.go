c280427985c7db5e3bd17ee3be75e5ce8f71367c
profadevairvitorio <profadevairvitorio@gmail.com>
2024-01-18 00:19:06 -0300

    chore: create example conversao.go

--- cap5/conversao.go
+++ cap5/conversao.go
@@ -4,18 +4,34 @@ import "fmt"
 
 type ListaDeCompras []string
 
-func imprimirSlice(slice []string) {
-	fmt.Println("Slice:", slice)
-}
+func (lista ListaDeCompras) Categorizar() ([]string, []string, []string) {
+	vegetais, carnes, outros := []string{}, []string{}, []string{}
 
-func imprimirLista(lista ListaDeCompras) {
-	fmt.Println("Lista de compras:", lista)
+	for _, e := range lista {
+		switch e {
+		case "Alface", "Pepino":
+			vegetais = append(vegetais, e)
+		case "Atum", "Frango":
+			carnes = append(carnes, e)
+		default:
+			outros = append(outros, e)
+		}
+	}
+
+	return vegetais, carnes, outros
 }
 
 func main() {
-	lista := ListaDeCompras{"Alface", "Atum", "Azeite"}
-	slice := []string{"Alface", "Atum", "Azeite"}
+	lista := make(ListaDeCompras, 6)
+	lista[0] = "Alface"
+	lista[1] = "Pepino"
+	lista[2] = "Azeite"
+	lista[3] = "Atum"
+	lista[4] = "Frango"
+	lista[5] = "Chocolate"
 
-	imprimirSlice([]string(lista))
-	imprimirLista(ListaDeCompras(slice))
+	vegetais, carnes, outros := lista.Categorizar()
+	fmt.Println("Vegetais:", vegetais)
+	fmt.Println("Carnes:", carnes)
+	fmt.Println("Outros:", outros)
 }
